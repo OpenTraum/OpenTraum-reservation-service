8230d0219f149b85edb30e04b18ad2eb55327cee
Suhyen Im <suhyenim.kor@gmail.com>
2026-04-07 13:43:25 +0900

    fix: RedisConfig Bean 충돌 및 Redisson AUTH 에러 수정 (#12)
    fix: #11 RedisConfig Bean 충돌 및 Redisson AUTH 에러 수정

--- src/main/java/com/opentraum/reservation/config/RedisConfig.java
+++ src/main/java/com/opentraum/reservation/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.opentraum.reservation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
+    @Primary
     public ReactiveStringRedisTemplate reactiveStringRedisTemplate(
             ReactiveRedisConnectionFactory connectionFactory) {
         return new ReactiveStringRedisTemplate(connectionFactory);
